7738349d33395ad95b933592fe0d1bbfd0e62b5a
gary <gary>
2024-07-31 10:43:16 +0800

    rendered and updated 07-31-2024-10-42

--- stacks/_config0_configs/kafka_on_ec2/_main/run.py
+++ stacks/_config0_configs/kafka_on_ec2/_main/run.py
@@ -344,7 +344,7 @@ class Main(newSchedStack):
 
     def run(self):
 
-        self.stack.unset_parallel()
+        self.stack.unset_parallel(sched_init=True)()
         self.add_job("sshkey")
         self.add_job("bastion")
         self.add_job("create")
