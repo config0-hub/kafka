429b2b7e48b5463694badfe97bcf183c248429e1
gary <gary>
2024-12-17 11:31:56 +0800

    rendered and updated 12-17-2024-11-31

---
+++ execgroups/_config0_configs/ubuntu_vendor_setup/_actions/25-setup_ansible.py
@@ -0,0 +1,7 @@
+def default():
+
+    task = {'method': 'shelloutconfig',
+            'metadata': {'env_vars': [],
+                         'shelloutconfigs': ['config0-publish:::kafka::create_ansible_replica_hosts']}}
+
+    return task
